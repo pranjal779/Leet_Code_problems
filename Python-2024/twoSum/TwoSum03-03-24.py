1ac4c7e270b908096371d34f08ff35e029047b20
Pranjal Shukla <pranjal779@gmail.com>
2024-03-03 11:17:17 -0500

    Update TwoSum03-03-24.py

--- Python-2024/twoSum/TwoSum03-03-24.py
+++ Python-2024/twoSum/TwoSum03-03-24.py
@@ -1,1 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        emptyList_for_req_num = {}
+        for i, num in enumerate(nums):
+            for_req_num = target - nums[i]
+            if for_req_num in emptyList_for_req_num:
+                return [emptyList_for_req_num[for_req_num], i]
+            emptyList_for_req_num[nums[i]] = i
+
+        return []
 
