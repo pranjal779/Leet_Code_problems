636d55012e4ece5ae41f2e6fe25faebf44ef1eeb
Pranjal Shukla <pranjal779@gmail.com>
2024-12-22 14:14:56 +0530

    07_1068_Product Sales Analysis I

--- SQL 50/07_1068_Product Sales Analysis I.sql
+++ SQL 50/07_1068_Product Sales Analysis I.sql
@@ -4,6 +4,13 @@ from Sales, Product
 -- left join Sales on Product
 where Product.product_id = Sales.product_id;
 
+
+-- or
+# Write your MySQL query statement below
+select Product.product_name, Sales.year, Sales.price
+from Product
+join Sales on Product.product_id = Sales.product_id;
+
 -- Table: Sales
 
 -- +-------------+-------+
