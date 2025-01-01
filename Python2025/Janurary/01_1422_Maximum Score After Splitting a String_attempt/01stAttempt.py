500178c80a0573371f3c27fb7cb6a5b5adf42cf8
Pranjal Shukla <pranjal779@gmail.com>
2025-01-01 20:45:40 +0530

    Update 01stAttempt.py

--- Python2025/Janurary/01_1422_Maximum Score After Splitting a String_attempt/01stAttempt.py
+++ Python2025/Janurary/01_1422_Maximum Score After Splitting a String_attempt/01stAttempt.py
@@ -1,3 +1,19 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        ones = s.count("1")
+        zeros = 0
+        ans = 0
+
+        for i in range(len(s) - 1):
+            if s[i] == "1":
+                ones -= 1
+            else:
+                zeros += 1
+
+
+            ans = max(ans, zeros + ones)
+
+        return ans
 '''
 Given a string s of zeros and ones, return the maximum score after splitting the string into two non-empty substrings (i.e. left substring and right substring).
 
