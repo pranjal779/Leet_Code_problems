ac0fdbc579b4621ffd7bf014545023d78a569674
Pranjal Shukla <pranjal779@gmail.com>
2025-01-04 23:19:04 +0530

    31_1789_Primary Department for Each Employee

---
+++ SQL 50/31_1789_Primary Department for Each Employee.sql
@@ -0,0 +1,9 @@
+-- # Write your MySQL query statement below
+SELECT employee_id, department_id
+FROM Employee
+WHERE primary_flag = "Y"
+UNION DISTINCT
+SELECT employee_id, department_id
+FROM Employee
+Group BY 1
+HAVING COUNT(*) = 1;
