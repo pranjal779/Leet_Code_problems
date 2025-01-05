9b5f2c78a713df0d0fd43847a75457f96c82c567
Pranjal Shukla <pranjal779@gmail.com>
2025-01-05 11:35:13 +0530

    33_180_Consecutive Numbers

---
+++ SQL 50/33_180_Consecutive Numbers.sql
@@ -0,0 +1,6 @@
+-- # Write your MySQL query statement below
+select distinct l1.Num as ConsecutiveNums
+from Logs l1, Logs l2, Logs l3
+where l1.Num = l2.Num and l2.Num = l3.Num
+and l1.id = l2.id - 1 
+and l1.id = l3.id - 2;
